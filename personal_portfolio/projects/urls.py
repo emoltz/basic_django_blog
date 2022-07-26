904144c5ff43946ea307cad7d25720fef0ef306c
emoltz <es5541@nyu.edu>
2022-07-26 09:54:56 -0400

    admin page working

--- personal_portfolio/projects/urls.py
+++ personal_portfolio/projects/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.project_index, name="project_index"),
+    path('home', views.home, name="home"),
     path("<int:pk>/", views.project_detail, name="project_detail"),
 ]
--- personal_portfolio/projects/views.py
+++ personal_portfolio/projects/views.py
@@ -1,19 +1,18 @@
 from django.shortcuts import render
-
-from models import Project
+from projects.models import Project
 
 
 def project_index(request):
     projects = Project.objects.all()
-    context = {
-        'projects': projects
-    }
-    return render(request, 'project_index.html', context)
+    context = {"projects": projects}
+    return render(request, "projects/project_index.html", context)
+
+
+def home(request):
+    return render(request, "projects/project_home.html", {})
 
 
 def project_detail(request, pk):
     project = Project.objects.get(pk=pk)
-    context = {
-        'project': project
-    }
-    return render(request, 'project_detail.html', context)
+    context = {"project": project}
+    return render(request, "projects/project_detail.html", context)
