abebb365bcd46aa20e57da366329c3aebed8d81d
emoltz <es5541@nyu.edu>
2022-07-26 11:26:42 -0400

    full blog!

--- personal_portfolio/blog/models.py
+++ personal_portfolio/blog/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -11,6 +15,10 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='posts')
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
 
 
 class Comment(models.Model):
